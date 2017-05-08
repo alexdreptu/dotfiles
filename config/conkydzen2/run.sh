fb3554e055aa2cf2ca74f8ca1ae81028b2f12997
Alexandru Dreptu <alexdreptu@gmail.com>
2017-05-08 19:31:05 +0300

    remove i3 and conkydzen2

--- config/conkydzen2/run.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-killall -q conky
-while pgrep -x conky > /dev/null; do sleep 1; done
-conky -c $HOME/.config/conkydzen2/conkydzen2.rc | dzen2 -dock -ta -l -fn "dejavu sans:bold:size=11:antialias=true" -bg "#272822" -xs 1 &
